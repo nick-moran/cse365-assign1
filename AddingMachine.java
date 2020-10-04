28b51d52ddffe676fa29aba38e34c5ab532615e7
Nick Moran <nick42791@gmail.com>
2020-10-04 15:57:07 -0700

    add history string/fill out methods

--- AddingMachine.java
+++ AddingMachine.java
@@ -10,41 +10,50 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
-  
+  private String history;
   /**
-   * This is the constructor. It takes no arguments, but sets total to zero.
+   * This is the constructor. It takes no arguments, but sets total to zero
+   * and sets history to ""
    */
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = "0";
   }
   
   /**
-   * This returns the total.
+   * Returns the total
+   * @return returns the total.
    */
   public int getTotal () {
-    return 0;
+    return this.total;
   }
 
   /**
    * This takes in a value and adds it to the total
+   * it also writes a plus to the string history
    * @param value This is the number that will be added to the total
    */
   public void add (int value) {
+    this.total += value;
+    this.history = this.history + " + " + value;
   }
 
   /**
    * This takes in a value and subtracts it from the total
+   * it also writes a subtract to the string history
    * @param value This is the number that will be subtracted
    */
   public void subtract (int value) {
+    this.total -= value;
+    this.history = this.history + " - " + value;
   }
 
   /**
-   * This returns the value as a string
-   * @return value as a string
+   * This returns the string history
+   * @return history as a string
    */
   public String toString () {
-    return "";
+    return this.history;
   }
 
   /**
@@ -52,5 +61,7 @@ public class AddingMachine {
    * setting total to zero
    */
   public void clear() {
+    this.total = 0;
+    this.history = "0";
   }
 }
